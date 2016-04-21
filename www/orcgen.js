6cf3c538f9b62bffe6b3f7a9f6ae38717b1d4045
Zachary Vance <za3k@za3k.com>
2016-04-20 21:45:26 -0700

    Add orc-themed subdomains

--- www/orcgen.js
+++ www/orcgen.js
@@ -1,7 +1,12 @@
+"use strict";
 // http://stackoverflow.com/questions/9071573/is-there-a-simple-way-to-make-a-random-selection-from-an-array-in-javascript-or
 function choose(choices) {
     var index = Math.floor(Math.random() * choices.length);
     return choices[index];
+}
+
+function repeat(e, n) {
+    return Array.apply(null, Array(n)).map(function(){return e;})
 }
 
 function generate_random(tree, seed) {
@@ -43,23 +48,27 @@ function acceptable_orc(orc) {
     return true;
 }
 
+var orctree = {
+    "orc(s)": ["<number> <orcs>", "<single orc>", "the last <single orc>"],
+    "orcs": ["<single orc>s"],
+    "single orc": ["<adjective> <single orc>", "orc"],
+    "adjective": ["smelly", "green", "big", "small", "dumb", "blue", "brown", "gross", "cross-eyed", "blood thirsty", "pretty", "slightly intimidating", "poop"],
+    "number": ["10000", "73", "<specific number>", "<lots> of", "<generic number>"],
+    "generic number": ["<lots> of", "hordes of", "a horde of", "many", "some", "a few", "quite a lot of", "tons of"],
+    "specific number": numbers(2, 99),
+    "huge number": numbers(1000000,9999999),
+    "lots": ["lots", "lots and <lots>"],
+};
 function generate_orc() {
-    orctree = {
-        "orc(s)": ["<number> <orcs>", "<single orc>", "the last <single orc>"],
-        "orcs": ["<single orc>s"],
-        "single orc": ["<adjective> <single orc>", "orc"],
-        "adjective": ["smelly", "green", "big", "small", "dumb", "blue", "brown", "gross", "cross-eyed", "blood thirsty", "pretty", "slightly intimidating", "poop"],
-        "number": ["10000", "73", "<specific number>", "<lots> of", "<generic number>"],
-        "generic number": ["<lots> of", "hordes of", "a horde of", "many", "some", "a few", "quite a lot of", "tons of"],
-        "specific number": numbers(2, 99),
-        "lots": ["lots", "lots and <lots>"],
-    };
     do { 
         var orc = generate_random(orctree, "<orc(s)>");
     }
     while (!acceptable_orc(orc));
     return orc;
 };
+function generate_collision_free_orc() {
+    return generate_random(orctree, "<huge number> <orcs>");
+}
 
 function generate_orc_emails(n) {
     n = n || 1;
@@ -78,13 +87,52 @@ function generate_orc_emails(n) {
     return emails;
 };
 
+function generate_password() {
+    var passtree = {
+        "password": [repeat("<symbol>", 20).join("")],
+        "symbol": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    }
+    var password = generate_random(passtree, "<password>");
+    return password;
+}
+
+function generate_orc_domain(password, subdomain) {
+    var subdomain = subdomain || generate_collision_free_orc().replace(/ /g, "");
+    var domain = subdomain + ".moreorcs.com";
+    var url = "https://ddns.za3k.com/?domain=" + subdomain;
+    if (password) {
+        password = generate_password();
+        url = url + "&password=" + password;
+    }
+    var curl4 = "curl -4 \"" + url + "\"";
+    var curl6 = "curl -6 \"" + url + "\"";
+    var curl = "curl \"" + url + "\"";
+    var frequency = "*/15        *          *             *                *            "
+    var shutup = ">/dev/null 2>/devnull"
+    var cron4 = frequency + "/usr/bin/curl -4 \"" + url + "\" " + shutup;
+    var cron6 = frequency + "/usr/bin/curl -6 \"" + url + "\" " + shutup;
+    var cron = frequency + "/usr/bin/curl \"" + url + "\" " + shutup;
+    return {
+        "password": password,
+        "subdomain": subdomain,
+        "domain": domain,
+        "url": url,
+        "curl4": curl4,
+        "curl6": curl6,
+        "curl": curl,
+        "cron4": cron4,
+        "cron6": cron6,
+        "cron": cron,
+    };
+}
+
 // http://ejohn.org/blog/javascript-micro-templating/
 // Simple JavaScript Templating
 // John Resig - http://ejohn.org/ - MIT Licensed
 (function(){
   var cache = {};
  
-  this.tmpl = function tmpl(str, data){
+  window.tmpl = function tmpl(str, data){
     // Figure out if we're getting a template, or if we need to
     // load the template - and be sure to cache the result.
     var fn = !/\W/.test(str) ?
